e0c561df32445055118ef88a3fbdbb1db55eee0a
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-03 11:34:09 -0300

    Add: urlsApi

---
+++ api/src/utils/urlsApi.js
@@ -0,0 +1,7 @@
+const URL_API_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+const URL_API_POKEMON_TYPE = "https://pokeapi.co/api/v2/type";
+
+module.exports = {
+  URL_API_POKEMON,
+  URL_API_POKEMON_TYPE,
+};
