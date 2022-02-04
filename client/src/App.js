38872177de60f99f1b3a67ffe36d512826be1355
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-04 20:34:26 -0300

    Add: App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import "./App.css";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
+import PokemonCreate from "./components/PokemonCreate";
+import PokemonDetail from "./components/PokemonDetail";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+          <Route path="/pokemon" component={PokemonCreate} />
+          <Route path="/home/:id" component={PokemonDetail} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
