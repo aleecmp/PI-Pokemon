9c47831405408a62c28464c01beb852af198bedd
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-06 17:08:52 -0300

    Fix cards

--- client/src/components/PokemonCard.jsx
+++ client/src/components/PokemonCard.jsx
@@ -1,9 +1,3 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { postPokemon } from "../redux/actions";
-import { Link, useHistory } from "react-router-dom";
-
 const PokemonCard = () => {
   return <div></div>;
 };
--- client/src/components/PokemonDetail.jsx
+++ client/src/components/PokemonDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from "../redux/actions";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
@@ -13,7 +13,23 @@ const PokemonDetail = (props) => {
 
   const myPokemon = useSelector((state) => state.detail);
 
-  return <div></div>;
+  return (
+    <div>
+      {myPokemon.name > 0 ? (
+        <div>
+          <h1>{myPokemon.name}</h1>
+          <img src={myPokemon.sprites.front_default} alt={myPokemon.name} />
+          <p>{myPokemon.weight}</p>
+          <p>{myPokemon.height}</p>
+          <p>{myPokemon.types[0].type.name}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+
+      <Link to="/home">Back</Link>
+    </div>
+  );
 };
 
 export default PokemonDetail;
