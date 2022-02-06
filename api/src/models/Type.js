8021591709d0eea0d030e4db9ffc8171db69fa54
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-06 17:10:03 -0300

    Fix remodel backend

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -4,13 +4,14 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("type", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
+    // id: {
+    //   type: DataTypes.INTEGER,
+    //   autoIncrement: true,
+    //   primaryKey: true,
+    // },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   });
 };
