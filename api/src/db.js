b9a2e3c8a132d38bd648d7ea075d118fa0abf894
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-05 23:35:08 -0300

    Fix db.js

--- api/src/db.js
+++ api/src/db.js
@@ -41,8 +41,8 @@ const { Pokemon, Type } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Pokemon.belongsToMany(Type, { through: "pokemon_type" });
-Type.belongsToMany(Pokemon, { through: "pokemon_type" });
+Pokemon.belongsToMany(Type, { through: "pokemons_type" });
+Type.belongsToMany(Pokemon, { through: "pokemons_type" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
