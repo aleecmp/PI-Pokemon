b477914db0fe778a0ef02046592f14e7963918d6
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-08 17:35:07 -0300

    Fix: minor fix

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,13 @@ import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
