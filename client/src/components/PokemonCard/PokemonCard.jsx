99dd4788b0f7e70660649160aefbf7f87bd90e72
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-13 22:29:08 -0300

    Add PokemonCard css

--- client/src/components/PokemonCard/PokemonCard.jsx
+++ client/src/components/PokemonCard/PokemonCard.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./PokemonCard.module.css";
 
 const PokemonCard = (props) => {
   console.log(props.id);
   console.log(props.name);
 
   return (
-    <div>
+    <div className={styles.pokeCard}>
       <Link to={`/details/${props.id}`}>
         <h1>{props.name}</h1>
       </Link>
-      <img src={props.sprite} alt={props.name} />
+      <div className={styles.allCards}>
+        <img
+          className={styles.pokeSprite}
+          src={props.sprite}
+          alt={props.name}
+        />
 
-      {props.types?.map((e) => (
-        <h5 key={e}>{e}</h5>
-      ))}
+        {props.types?.map((e) => (
+          <h5 key={e}>{e}</h5>
+        ))}
+      </div>
     </div>
   );
 };
